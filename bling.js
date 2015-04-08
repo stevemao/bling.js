ce72c739f5c942c6d32fdf663774a2d867256ee6
Paul Irish <paul.irish@gmail.com>
2015-04-07 19:26:19 -0700

    standard

--- bling.js
+++ bling.js
@@ -1,17 +1,17 @@
 // blinggggggggggg
 
-Window.prototype.$ = document.querySelectorAll.bind(document);
+Window.prototype.$ = document.querySelectorAll.bind(document)
 
 Node.prototype.on = function (name, fn) {
-	this.addEventListener(name, fn);
+  this.addEventListener(name, fn)
 }
 
 // NodeList needs all Array goodies
-NodeList.prototype.__proto__ = Array.prototype;
+NodeList.prototype.__proto__ = Array.prototype
 
 // NodeList needs EventTarget goodies but iterated
 NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn) {
-    this.forEach(function(elem, i) { 
-      elem.on(name, fn); 
-    });
-}; // todo: dispatchEvent & removeEventListener
+  this.forEach(function (elem, i) {
+    elem.on(name, fn)
+  })
+} // todo: dispatchEvent & removeEventListener
--- example.js
+++ example.js
@@ -1,18 +1,14 @@
-
-
 // We should be able to forEach on the qSA result.
-document.querySelectorAll('input').forEach(function(el){
-	el.readOnly = true
+document.querySelectorAll('input').forEach(function (el) {
+  el.readOnly = true
 })
 
 // addEventListhistakestoolongtotype. on() method
-document.body.on('dblclick', function(e){ 
-	console.log('double clicked the body');
+document.body.on('dblclick', function (e) {
+  console.log('double clicked the body')
 })
 
 // classic Bling + on()
-$('p').on('click', function(e){
-    p.hidden = true;
-});
-
-
+$('p').on('click', function (e) {
+  p.hidden = true
+})
