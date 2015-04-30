b8116cc0cdaef351b9aa1680fd905f427e35a3b3
Paul Irish <paul.irish@gmail.com>
2015-04-30 16:24:27 -0700

    cleenr

--- bling.js
+++ bling.js
@@ -1,15 +1,13 @@
 // bling
-// the kind of thing you toss into the page as you start prototyping
-// also a prollyfill.
 
-//   it's all resig's fault.  github.com/jeresig/nodelist
+// the kind of thing you toss into the page as you start prototyping
+
 
 Window.prototype.$ = document.querySelectorAll.bind(document)
 
-Node.prototype.on = function (name, fn) {
+Node.prototype.on = Window.prototype.on = function (name, fn) {
   this.addEventListener(name, fn)
 }
-// setting to EventTarget.prototype.addEventListener; directly would be nice, but not supported everywhere.
 
 // NodeList needs all Array goodies
 NodeList.prototype.__proto__ = Array.prototype
@@ -19,7 +17,12 @@ NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn
   this.forEach(function (elem, i) {
     elem.on(name, fn)
   })
-} // todo: dispatchEvent & removeEventListener
+} // 
 
 
 
+
+// Notes:
+//   it's all resig's fault.  github.com/jeresig/nodelist
+//   setting `Node.prototype.on = EventTarget.prototype.addEventListener` would be sweet but, no funciona in IE/Saf
+//   not done: dispatchEvent & removeEventListener shortcuts, but who caaares
